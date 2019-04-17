96060fd56e17b23c6b390009e3ef38c339fed789
xcy <2698064198@qq.com>
2019-04-17 10:52:32 +0800

    自定义后台管理页面 关联注册 更改后台对象列名显示

--- demo1/booktest/admin.py
+++ demo1/booktest/admin.py
@@ -2,5 +2,31 @@ from django.contrib import admin
 from .models import BookInfo,HeroInfo
 
 # Register your models here.
-admin.site.register(BookInfo)
-admin.site.register(HeroInfo)
+
+
+class HeroInfoInline(admin.StackedInline):
+    model = HeroInfo
+    # 关联个数
+    extra = 1
+
+
+class BookInfoAdmin(admin.ModelAdmin):
+    inlines = [HeroInfoInline]
+
+
+admin.site.register(BookInfo, BookInfoAdmin)
+
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    # 显示字段，点击列头可以排序
+    list_display = ['name', 'sex', 'skill']
+    # 过滤字段，过滤框出现在右侧
+    list_filter = ['hgender']
+    # 分页，分页框出现在下侧
+    list_per_page = 2
+    # 搜索字段，搜索框出现在上侧
+    search_fields = ['hname']
+
+
+admin.site.register(HeroInfo, HeroInfoAdmin)
+
--- demo1/booktest/models.py
+++ demo1/booktest/models.py
@@ -18,3 +18,18 @@ class HeroInfo(models.Model):
     def __str__(self):
         return self.hname
 
+    def name(self):
+        return self.hname
+
+    name.short_description = '角色'
+
+    def sex(self):
+        return self.hgender
+
+    sex.short_description = '性别'
+
+    def skill(self):
+        return self.hcontent
+
+    skill.short_description = '绝招'
+
