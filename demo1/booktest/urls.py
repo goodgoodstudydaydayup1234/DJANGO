221ca7702c1a275bee4998f04513aea9623bf3aa
xcy <2698064198@qq.com>
2019-04-17 16:53:00 +0800

    添加首页、列表页、详情页模板

--- demo1/booktest/urls.py
+++ demo1/booktest/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^index/$', views.index),
+    url(r'^$', views.index),
     url(r'^list/$', views.list),
     url(r'^detail/(\d+)/$', views.detail),
 ]
--- demo1/booktest/views.py
+++ demo1/booktest/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import BookInfo
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse('首页')
+
+    return render(request, 'booktest/index.html', {'username': 'xcy'})
+    # temp = loader.get_template('booktest/index.html')
+    # result = temp.render({'username': 'xcy'})
+    # return HttpResponse(result)
 
 
 def list(request):
-    return HttpResponse('列表页')
+    bl = BookInfo.objects.all()
+    return render(request, 'booktest/list.html', {'booklist': bl})
+    # temp = loader.get_template('booktest/list.html')
+    # result = temp.render({'bname': '连城诀', 'pub_time': '2000年'})
+    # return HttpResponse(result)
 
 
 def detail(request, id):
-    try:
-        return HttpResponse(BookInfo.objects.get(pk=int(id)))
-    # return HttpResponse('详情页'+str(id))
-    except:
-        return HttpResponse('请输入正确的图书id')
+    book = BookInfo.objects.get(pk=int(id))
+    return render(request, 'booktest/detail.html', {'book': book})
+    # try:
+    #     return HttpResponse(BookInfo.objects.get(pk=int(id)))
+    # # return HttpResponse('详情页'+str(id))
+    # except:
+    #     return HttpResponse('请输入正确的图书id')
 
