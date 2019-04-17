741e6600a642e3071c178426005f9c4d3e0e0ec3
xcy <2698064198@qq.com>
2019-04-17 21:04:17 +0800

    去除模板硬编码

--- demo1/demo1/urls.py
+++ demo1/demo1/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'', include('booktest.urls'))
+    url(r'', include('booktest.urls', namespace='booktest'))
 ]
