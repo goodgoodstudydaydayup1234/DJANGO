c5b4a8819550df3d2a75d3e9736f07861af23b0e
xcy <2698064198@qq.com>
2019-04-17 14:42:34 +0800

    添加详情页，并传参

--- demo1/booktest/urls.py
+++ demo1/booktest/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     url(r'^index/$', views.index),
     url(r'^list/$', views.list),
+    url(r'^detail/(\d+)/$', views.detail),
 ]
--- demo1/booktest/views.py
+++ demo1/booktest/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BookInfo
 
 
 def index(request):
@@ -8,4 +9,12 @@ def index(request):
 
 def list(request):
     return HttpResponse('列表页')
+
+
+def detail(request, id):
+    try:
+        return HttpResponse(BookInfo.objects.get(pk=int(id)))
+    # return HttpResponse('详情页'+str(id))
+    except:
+        return HttpResponse('请输入正确的图书id')
 
