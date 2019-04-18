055257e69090b26cce15b337b71b72ae8c12825e
xcy <2698064198@qq.com>
2019-04-18 16:45:13 +0800

    问题列表页和详情页

--- demo2/polls/views.py
+++ demo2/polls/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Question,Choose
 # Create your views here.
+
+
+def list(request):
+    queslist = Question.objects.all()
+    return render(request, 'polls/list.html', {'queslist': queslist})
+
+
+def detail(request, quesid):
+    print(111111)
+    ques = Question.objects.get(pk=quesid)
+    # chooses = Question.choose_set.get()
+    return render(request, 'polls/detail.html', {'ques': ques})
+
